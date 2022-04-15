1075ffbf81566fc13f5106c05551054c4d8b8cf6
meangpu <51825872+meangpu@users.noreply.github.com>
2022-04-16 04:54:20 +0700

    ww

---
+++ SmartBottle/Assets/__Scripts/ArduinoConnect.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+
+using UnityEngine;
+
+public class ArduinoConnect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
