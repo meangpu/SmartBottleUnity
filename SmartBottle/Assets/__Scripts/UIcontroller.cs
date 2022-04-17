d6cfc4a47f2e7a801273f758c8d871e8a4784f9a
meangpu <51825872+meangpu@users.noreply.github.com>
2022-04-17 19:15:34 +0700

    fin

--- SmartBottle/Assets/__Scripts/UIcontroller.cs
+++ SmartBottle/Assets/__Scripts/UIcontroller.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class UIcontroller : MonoBehaviour
 {
     [SerializeField] Slider slider;
     [SerializeField] int maxNumber = 13;  // max 20cm  but real bottle is 13
+
+    [SerializeField] GameObject blurVolumn;
+
+    [SerializeField] Transform creditUI;
+    [SerializeField] Transform mainUI;
+
+
+
 
 
     public void updateSlider(float v)
@@ -22,4 +31,28 @@ public class UIcontroller : MonoBehaviour
 
         slider.value = Mathf.Abs((v / maxNumber)-1) ; 
     }
+
+    public void CreditOn()
+    {
+        var sequence = DOTween.Sequence();
+
+        blurVolumn.SetActive(true);
+        sequence.Append(mainUI.DOLocalMoveY(-1000, .4f));
+        sequence.Append(creditUI.DOLocalMoveY(0, .45f));
+
+        
+    }
+
+    public void CreditOff()
+    {
+        var sequence = DOTween.Sequence();
+
+        blurVolumn.SetActive(false);
+        sequence.Append(creditUI.DOLocalMoveY(955, .4f));
+        sequence.Append(mainUI.DOLocalMoveY(0, .45f));
+
+        
+    }
+
+
 }
